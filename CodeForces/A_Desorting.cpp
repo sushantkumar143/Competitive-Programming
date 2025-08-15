aa65367e2a3aeeb92e3accd21a547f4ab97c7d90
sushantkumar143 <kumarsushant200641@gmail.com>
2025-08-15 14:56:01 +0530

    Questions added

--- CodeForces/A_Desorting.cpp
+++ CodeForces/A_Desorting.cpp
@@ -1,23 +1,64 @@
-#include <iostream>
-#include<bits/stdc++.h>
+// #include <iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int times;
+//     cin >> times;
+
+//     while(times--){
+//         int n;
+//         cin >> n;
+//         vector<int> arr(n);
+//         for(int i=0; i<n; i++){
+//             cin >> arr[i];
+//         }
+//         int diff = INT_MAX;
+//         for(int i=1; i<n; i++){
+//             diff = min(diff, arr[i]-arr[i-1]);
+//         }
+//         int ans = (diff/2) + 1;
+//         cout << ans << "\n";
+//     }
+// }
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int times;
-    cin >> times;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    while(times--){
+    int t;
+    cin >> t;
+    while (t--) {
         int n;
         cin >> n;
-        vector<int> arr(n);
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        bool sorted = true;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                sorted = false;
+                break;
+            }
         }
-        int diff = INT_MAX;
-        for(int i=1; i<n; i++){
-            diff = min(diff, arr[i]-arr[i-1]);
+        if (!sorted) {
+            cout << 0 << "\n";
+            continue;
         }
-        int ans = (diff/2) + 1;
+
+        long long ans = LLONG_MAX;
+        for (int i = 0; i < n - 1; i++) {
+            long long diff = a[i + 1] - a[i];
+            long long ops = diff / 2 + 1;
+            ans = min(ans, ops);
+        }
+
         cout << ans << "\n";
     }
+    return 0;
 }
